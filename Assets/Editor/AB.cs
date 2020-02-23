5730b8c532860a7e2fc957e6e5770084a895d9b3
Dmitry-dev777 <57875882+Dmitry-dev777@users.noreply.github.com>
2020-02-23 13:43:38 +0200

    2

---
+++ Assets/Editor/AB.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AB : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetsBudles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }      
+
+}
